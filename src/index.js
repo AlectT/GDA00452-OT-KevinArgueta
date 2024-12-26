b621c2a1d0009fdb1f76ba363eafd71d0626d790
KevinAr <kevinargueta296@gmail.com>
2024-12-25 20:32:12 -0600

    Cracion de middlewares y uso de cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-import express, { Router } from 'express';
+import express from 'express';
 import routerUsuario from './rutas/rutasUsuario.js';
 import routerRol from './rutas/rutasRol.js';
 import routerEstado from './rutas/rutasEstado.js';
@@ -8,15 +8,17 @@ import routerCategoria from './rutas/rutasCategoria.js';
 import routerOrden from './rutas/rutasOrden.js';
 import routerProducto from './rutas/rutasProducto.js';
 import routerDetalles from './rutas/rutasDetalles.js';
-import jsonwebtoken from 'jsonwebtoken';
 import keys from './config/keys.js';
-import sequelize from './config/conexion.js';
-import { QueryTypes } from '@sequelize/core';
-import bcrypt from 'bcrypt';
+import cors from 'cors';
+import verificacionToken from './middleware/verificacionToken.js';
+import iniciarSesion from './middleware/IniciarSesion.js';
 
 dotenv.config();
 
 const app = express();
+
+// Utilizacion del metodo cors para consumir el API
+app.use(cors());
 
 //Configuracion de express
 app.use(express.urlencoded({ extended: false }));
@@ -26,98 +28,12 @@ app.use(express.json());
 app.set('keyOperador', keys.keyOperador);
 app.set('keyCliente', keys.keyCliente);
 
-// MiddleWare para JWT
-app.post('/iniciarSesion', async (req, res) => {
-	try {
-		const data = req.body;
-		// Obtener el correo con el que se quiere iniciar sesion
-		await sequelize
-			.query('exec pBuscarUsuarioCorreo @correo = :correo;', {
-				replacements: {
-					correo: data.correo,
-				},
-				type: QueryTypes.SELECT,
-			})
-			.then(async (usuario) => {
-				// Validacion existe un usuario con ese correo
-				if (!usuario[0]) {
-					return res.status(201).send('Datos incorrectos');
-				} else {
-					// Verificaciones de la contraseña y el correo
-					const hashSaved = usuario[0].password;
-					const isPasswordValid = await bcrypt.compare(data.password, hashSaved);
-					if (isPasswordValid && data.correo === usuario[0].correo_electronico) {
-						// Creacion del token
-						const payload = {
-							check: true,
-						};
-						if (usuario[0].idRol === 1) {
-							const token = jsonwebtoken.sign(payload, app.get('keyCliente'), {
-								expiresIn: '1d',
-							});
-							return res.status(201).json({
-								mensaje: 'Autenticacion exitosa',
-								token: token,
-							});
-						} else if (usuario[0].idRol === 2) {
-							const token = jsonwebtoken.sign(payload, app.get('keyOperador'), {
-								expiresIn: '1d',
-							});
-							return res.status(201).json({
-								mensaje: 'Autenticacion exitosa',
-								token: token,
-							});
-						}
-					} else {
-						//Si los datos no son correctos no se hará el token
-						return res.status(201).json({
-							mensaje: 'Datos incorrectos',
-						});
-					}
-				}
-			});
-	} catch (err) {
-		console.log('Error durante el proceso');
-		return res.status(500).send(err);
-	}
-});
+// MiddleWare para iniciar sesion
+app.use(iniciarSesion);
 
-// MiddleWare para verificar el token
-const verificacion = Router();
-
-verificacion.use((req, res, next) => {
-	let token = req.headers['x-acces-token'] || req.headers['authorization'];
-	if (!token) {
-		return res.status(401).send('Se necesita el token para la autenticación');
-	}
-
-	if (token.startsWith('Bearer ')) {
-		token = token.replace('Bearer ', '');
-	}
-
-	if (token) {
-		jsonwebtoken.verify(token, app.get('keyOperador'), (errO, decodedO) => {
-			if (!errO) {
-				req.rol = 'O';
-				req.decoded = decodedO;
-				next();
-			} else {
-				jsonwebtoken.verify(token, app.get('keyCliente'), (errC, decodedC) => {
-					if (!errC) {
-						req.rol = 'C';
-						req.decoded = decodedC;
-						next();
-					} else {
-						return res.json({ mensaje: 'El token no es valido' });
-					}
-				});
-			}
-		});
-	}
-});
-
+// MiddleWare de validacion de Token
+app.use(verificacionToken);
 // Separacion de rutas por tablas
-app.use(verificacion);
 app.use(routerUsuario);
 app.use(routerRol);
 app.use(routerEstado);
@@ -129,3 +45,4 @@ app.use(routerDetalles);
 
 //Inicializacion de servidor
 app.listen(4000);
+export default app;
